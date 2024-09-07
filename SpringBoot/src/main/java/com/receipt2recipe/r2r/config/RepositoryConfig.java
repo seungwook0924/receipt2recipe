8cca084e09ca655eee8054ba1dbbbb2d13c58ec4
SeungWook_Lee <leesw_0924@icloud.com>
2024-09-07 19:32:25 +0900

    git ignore update & properties -> yml

--- SpringBoot/src/main/java/com/receipt2recipe/r2r/config/RepositoryConfig.java
+++
@@ -1,9 +0,0 @@
-package com.receipt2recipe.r2r.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(basePackages = "com.receipt2recipe.r2r.repository")
-public class RepositoryConfig {
-}
