60ca3ec557455f34a50573de9438a176bb1c69a6
Lee seungwook <leesw_0924@icloud.com>
2025-03-25 20:44:13 +0900

    ♻️ Reapply .gitignore rules

--- SpringBoot/src/main/java/com/receipt2recipe/r2r/exception/UniqueConstraintViolationException.java
+++
@@ -1,7 +0,0 @@
-package com.receipt2recipe.r2r.exception;
-
-public class UniqueConstraintViolationException extends RuntimeException {
-    public UniqueConstraintViolationException(String message) {
-        super(message);
-    }
-}
